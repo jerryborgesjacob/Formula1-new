31a215295180f6cdef7ba748c26b6130cd783874
Jerry Borges <jerryborgesj@gmail.com>
2024-06-14 12:24:09 -0400

    Add project files.

---
+++ Formula1-new/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Formula1_new.MvcApplication" Language="C#" %>
---
+++ Formula1-new/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Formula1_new.Startup))]
+namespace Formula1_new
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
